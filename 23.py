81af1987f1cabe2a6b347c4704f2fbee63b59891
d1asv <timoxa-io@ukr.net>
2020-12-01 22:20:05 +0200

    coc

---
+++ 23.py
@@ -0,0 +1,25 @@
+h = open("E:\ICS-123\my_file\my_file.txt", 'w+')
+i = 1
+for i in range(10):
+    
+    h.write(str(i) + " строка\n")
+    
+    i + 1
+    
+h.close()
+
+b = open("E:\ICS-123\my_file\my_file.txt", 'r')
+slist = []
+k = 0
+for k in range (5):
+    stroka = b.readlines(k)
+    slist.append(stroka)
+    print(slist[k])
+    k+1
+b.close()
+
+print('___________________________________')
+
+with open("E:\ICS-123\my_file\my_file.txt") as sf:
+    file = sf.readlines()
+print(file)
